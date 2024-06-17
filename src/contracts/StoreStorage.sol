e26c10cc112cab40a64c0adb138c128d81848e0e
MiriTenenboim <miryamt@darkeychana.com>
2024-06-17 17:12:27 +0300

    contracts - store

--- src/contracts/StoreStorage.sol
+++ src/contracts/StoreStorage.sol
@@ -1,4 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-contract 
+struct Constant {
+    uint256 constant BPS_DIVIDER;
+    uint256 constant MAX_FEE;
+    uint256 constant MAX_KEEPER_FEE_SHARE;
+    uint256 constant MAX_POOL_WITHDRAWAL_FEE;
+    uint256 constant FUNDING_INTERVAL;
+}
+
+
+struct Contract {
+    address gov;
+    address currency;
+    address clp;
+
+    address swapRouter;
+    address quoter;
+    address weth;
+
+    address trade;
+    address pool;
+}
