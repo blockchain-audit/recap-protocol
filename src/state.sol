3be247e0672d1edd5998395ceee85a30f081d33c
shaniBorek <shaniborek@gmail.com>
2024-06-18 12:36:41 +0300

    update

---
+++ src/state.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+import "./interfaces/IChainlink .sol";
+contract A{
+    uint public s;
+
+}
+
+// struct pool {
+//     uint256  BPS_DIVIDER;
+//     address  gov;
+//     address  trade;
+//     address  treasury;
+
+//     IStore  store;
+
+
+// }
+
+// struct CLP{
+//      address store;
+// }
+
+struct chinLink{
+    uint256  UNIT 
+    ;
+    uint256 GRACE_PERIOD_TIME  ;
+
+}
