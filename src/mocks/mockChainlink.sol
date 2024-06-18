1b1cbf2b91e97ddd410e3c2b50af74f041db185b
oritGreenvald <121660885+oritlu10@users.noreply.github.com>
2024-06-18 11:41:38 +0300

    Create mockChainlink.sol

---
+++ src/mocks/mockChainlink.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.24;
+
+contract MockChainlink {
+    mapping(address => uint256) prices;
+
+    constructor() {}
+
+    function setPrice(address feed, uint256 price) external {
+        prices[feed] = price;
+    }
+
+    function getPrice(address feed) external view returns (uint256) {
+        return prices[feed];
+    }
+}
