e73d7026e4deb3e4232b3708f63b3024459c4514
shaniBorek <shaniborek@gmail.com>
2024-06-18 14:57:29 +0300

    chainlink

--- src/state.sol
+++ src/state.sol
@@ -1,29 +1,26 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 import "./interfaces/IChainlink .sol";
+import "./interfaces/IStore.sol";
 contract A{
     uint public s;
 
 }
 
-// struct pool {
-//     uint256  BPS_DIVIDER;
-//     address  gov;
-//     address  trade;
-//     address  treasury;
+struct pool {
+    uint256  BPS_DIVIDER;
+    address  gov;
+    address  trade;
+    address  treasury;
+    IStore  store;
+}
 
-//     IStore  store;
-
-
-// }
-
-// struct CLP{
-//      address store;
-// }
+struct CLP{
+     address store;
+}
 
 struct chinLink{
-    uint256  UNIT 
-    ;
+    uint256  UNIT ;
     uint256 GRACE_PERIOD_TIME  ;
 
 }
