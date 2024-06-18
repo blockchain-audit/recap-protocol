8e2e2955c7aca90d1e9992851560f68849b7c645
YehudisSternberg <yhs52274@gmail.com>
2024-06-18 11:20:24 +0300

    save

--- src/StoreStorage.sol
+++ src/StoreStorage.sol
@@ -1,15 +1,29 @@
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/IQuoter.sol";
+
+import "./interfaces/IStore.sol";
+import "./interfaces/ICLP.sol";   
+   
+   
     using EnumerableSet for EnumerableSet.UintSet;
     using EnumerableSet for EnumerableSet.Bytes32Set;
     using EnumerableSet for EnumerableSet.AddressSet;
     using SafeERC20 for IERC20;
 
     // constants
-    uint256 public constant BPS_DIVIDER = 10000;
+    struct Constants {
+        uint256 public constant BPS_DIVIDER = 10000;
     uint256 public constant MAX_FEE = 500; // in bps = 5%
     uint256 public constant MAX_KEEPER_FEE_SHARE = 2000; // in bps = 20%
     uint256 public constant MAX_POOL_WITHDRAWAL_FEE = 500; // in bps = 5%
     uint256 public constant FUNDING_INTERVAL = 1 hours; // In seconds.
+    }
 
+    struct Contracts {
     // contracts
     address public gov;
     address public currency;
@@ -20,10 +34,12 @@
     address public weth;
 
     address public trade;
-    address public pool;
-
-    // Variables
-    uint256 public poolFeeShare = 5000; // in bps
+    address public pool;        
+    }
+    
+    struct Variables {
+            // Variables
+     uint256 public poolFeeShare = 5000; // in bps
     uint256 public keeperFeeShare = 1000; // in bps
     uint256 public poolWithdrawalFee = 10; // in bps
     uint256 public minimumMarginLevel = 2000; // 20% in bps, at which account is liquidated
@@ -52,8 +68,29 @@
 
     mapping(address => uint256) private balances; // user => amount
     mapping(address => uint256) private lockedMargins; // user => amount
-    EnumerableSet.AddressSet private usersWithLockedMargin; // [users...]
+    EnumerableSet.AddressSet private usersWithLockedMargin; // [users...]       
+    }
 
+    struct Funding {
     // Funding
     mapping(string => int256) private fundingTrackers; // market => funding tracker (long) (short is opposite) // in UNIT * bps
     mapping(string => uint256) private fundingLastUpdated; // market => last time fundingTracker was updated. In seconds.
+    }
+
+    struct State {
+    Funding funding;
+    Variables variables;
+    cContracts contracts;
+    Constants constants;
+    }
+
+    abstract contract StoreStorage {
+    State internal state;
+    }
+
+
+
+
+
+
+
