fa241552a4d9105d436b57314e9554137cbaf72f
YehudisSternberg <yhs52274@gmail.com>
2024-06-20 00:26:17 +0300

    check pool and finish the addLiquadity's functions

--- src/contracts/Pool.sol
+++ src/contracts/Pool.sol
@@ -7,19 +7,34 @@ import {AddLiquidityThroughUniswap} from "../libraries/actions/AddLiquidityThrou
 
 import {AddLiquidity} from "../libraries/actions/AddLiquidity.sol";
 
-contract Pool is CapStorage{
+contract Pool is CapStorage, IPool{
     using AddLiquidityThroughUniswap for State;
     using AddLiquidity for State;
 
-    function addLiquidity(uint256 amount) public payable {
+    function addLiquidity(uint256 amount) external {
         state.validateAddLiquidity(amount);
         state.executeAddLiquidity(amount);
     }
-  function addLiquidityThroughUniswap(address tokenIn, uint256 amountIn, uint256 amountOutMin, uint24 poolFee)
+    function addLiquidityThroughUniswap(address tokenIn, uint256 amountIn, uint256 amountOutMin, uint24 poolFee)
         external
         payable
     {
         state.validateAddLiquidityThroughUniswap(tokenIn, amountIn, amountOutMin, poolFee);
         state.executeAddLiquidityThroughUniswap(tokenIn, amountIn, amountOutMin, poolFee);
     }
+
+
+
+
+    function creditFee(address user, string memory market, uint256 fee, bool isLiquidation) external{}
+
+    function creditTraderLoss(address user, string memory market, uint256 amount) external{}
+
+    function debitTraderProfit(address user, string memory market, uint256 amount) external{}
+
+    function link(address _trade, address _store, address _treasury) external{}
+
+    function removeLiquidity(uint256 amount) external{}
+
+    function updateGov(address _gov) external{}
 } 
