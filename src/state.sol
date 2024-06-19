fd93b7d6772f2d970fc48bb1e8372938bb387bd7
shaniBorek <shaniborek@gmail.com>
2024-06-19 16:05:16 +0300

    wip

--- src/state.sol
+++ src/state.sol
@@ -1,26 +1,120 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
-import "./interfaces/IChainlink .sol";
-import "./interfaces/IStore.sol";
-contract A{
-    uint public s;
+pragma solidity ^0.8.20;
+import "@hack/interfaces/IChainlink.sol";
+import "@hack/interfaces/IPool.sol";
+import "@hack/interfaces/IStore.sol";
 
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+    using EnumerableSet for EnumerableSet.UintSet;
+    using EnumerableSet for EnumerableSet.Bytes32Set;
+    using EnumerableSet for EnumerableSet.AddressSet;
+
+struct ChainLink{
+    uint256  UNIT ;
+    uint256 GRACE_PERIOD_TIME  ;
+    AggregatorV3Interface sequencerUptimeFeed;
 }
 
-struct pool {
-    uint256  BPS_DIVIDER;
+struct Pool {
+    uint256 BPS_DIVIDER   ;
     address  gov;
     address  trade;
     address  treasury;
     IStore  store;
 }
 
-struct CLP{
-     address store;
+struct StoreConstants{
+    uint256  BPS_DIVIDER ;
+    uint256 MAX_FEE ; 
+    uint256 MAX_KEEPER_FEE_SHARE ; 
+    uint256 MAX_POOL_WITHDRAWAL_FEE ;
+    uint256 FUNDING_INTERVAL ; 
+
 }
 
-struct chinLink{
+struct StoreAddressContracts {
+    address  gov;
+    address  currency;
+    address  clp;
+    address  swapRouter;
+    address  quoter;
+    address  weth;
+    address  trade;
+    address  pool;
+}
+
+struct StoreVariables{
+    uint256  poolFeeShare;
+    uint256  keeperFeeShare ;
+    uint256  poolWithdrawalFee ;
+    uint256  minimumMarginLevel ;
+    uint256  bufferBalance;
+    uint256  poolBalance;
+    uint256  poolLastPaid;
+    uint256  bufferPayoutPeriod ;
+    uint256  orderId;
+}
+
+struct StoreMapping   {
+    mapping(uint256 => IStore.Order)  orders;
+    mapping(address => EnumerableSet.UintSet)  userOrderIds;    
+    mapping(string => IStore.Market)  markets;
+    mapping(bytes32 => IStore.Position)  positions; 
+    mapping(address => EnumerableSet.Bytes32Set)  positionKeysForUser; 
+    mapping(string => uint256)  OILong;
+    mapping(string => uint256)  OIShort;
+    mapping(address => uint256)  balances; 
+    mapping(address => uint256)  lockedMargins;  
+    mapping(string => int256)  fundingTrackers; 
+    mapping(string => uint256)  fundingLastUpdated; 
+
+}
+
+struct StoreStruct{ 
+     EnumerableSet.UintSet  orderIds;
+     EnumerableSet.Bytes32Set  positionKeys; 
+     EnumerableSet.AddressSet  usersWithLockedMargin;
+}
+
+struct StoreArr{
+    string[]  marketList; 
+}
+
+struct Trade { 
     uint256  UNIT ;
-    uint256 GRACE_PERIOD_TIME  ;
-
+    uint256  BPS_DIVIDER ;
+    address  gov;
+    IChainlink  chainlink;
+    IPool  pool;
+    IStore  store;
 }
+
+struct State {
+    ChainLink chinLink;
+    Pool pool;
+    StoreAddressContracts storeAddressContracts;
+    // StoreConstants storeConstants;
+    StoreVariables storeVariables;
+    StoreMapping storeMapping;
+    StoreStruct storeStruct;
+    StoreArr storeArr;
+    Trade trade;
+}
+
+abstract contract Storage {
+    State internal state;
+    
+    StoreConstants private constants  =  StoreConstants({
+        BPS_DIVIDER: 10000,
+        MAX_FEE : 500,
+        MAX_KEEPER_FEE_SHARE : 2000,
+        MAX_POOL_WITHDRAWAL_FEE : 500,
+        FUNDING_INTERVAL : 1 hours
+
+    });
+
+    
+}
+
