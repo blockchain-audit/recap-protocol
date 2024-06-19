018f08a8793484b2b34d95e1333de456e11af781
chana26252 <chana26252@gmail.com>
2024-06-19 19:01:38 +0300

    wip

---
+++ src/libraries/Math.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+
+library Math {
+
+    function calcClpAmount(uint amount, uint clpSupply, uint balance) internal pure returns (uint) {
+        return clpAmount = balance == 0 || clpSupply == 0 ? amount : amount * clpSupply / balance;
+    }
+        uint256 feeAmount = amount * store.poolWithdrawalFee() / BPS_DIVIDER;
+
+    function calcFeeAmount(uint )
+}
