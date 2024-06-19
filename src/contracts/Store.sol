7dd502ae68488f06ab3e1f14340fe9418327000d
sari341 <s6799152@gmail.com>
2024-06-19 16:17:51 +0300

    install uniswap

--- src/contracts/Store.sol
+++ src/contracts/Store.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
-import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
-import "@uniswap/v3-periphery/contracts/interfaces/IQuoter.sol";
+import "../../lib/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "../../lib/v3-periphery/contracts/interfaces/IQuoter.sol";
 
 import "../interfaces/IStore.sol";
 import "../interfaces/ICLP.sol";
