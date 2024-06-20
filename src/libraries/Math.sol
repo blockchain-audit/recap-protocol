5062b71266a71531429b430d4fe17b0626d5c5e2
chana26252 <chana26252@gmail.com>
2024-06-20 10:02:56 +0300

    wip

--- src/libraries/Math.sol
+++ src/libraries/Math.sol
@@ -7,7 +7,7 @@ library Math {
     function calcClpAmount(uint amount, uint clpSupply, uint balance) internal pure returns (uint) {
         return clpAmount = balance == 0 || clpSupply == 0 ? amount : amount * clpSupply / balance;
     }
-        uint256 feeAmount = amount * store.poolWithdrawalFee() / BPS_DIVIDER;
+        // uint256 feeAmount = amount * store.poolWithdrawalFee() / BPS_DIVIDER;
 
-    function calcFeeAmount(uint )
-}
+//     function calcFeeAmount(uint )
+// }
