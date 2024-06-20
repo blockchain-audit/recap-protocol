d9abe3e5fc97f4cba90fd1524fc0d76fafea0551
oritGreenvald <121660885+oritlu10@users.noreply.github.com>
2024-06-20 11:55:07 +0300

    Create pool.sol

---
+++ src/libraries/pool.sol
@@ -0,0 +1,21 @@
+
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.24;
+
+import "forge-std/console.sol";
+
+import {State} from "../contracts/CapStorage.sol";
+
+import {CLPToken} from "./CLPtoken.sol";
+
+import {Errors} from "./Errors.sol";
+
+library Pool {
+    function incrementPoolBalance(State storage state, uint256 amount) external {
+        state.balances.poolBalance += amount;
+    }
+
+    function decrementPoolBalance(State storage state, uint256 amount) external {
+        state.balances.poolBalance -= amount;
+    }
+}
