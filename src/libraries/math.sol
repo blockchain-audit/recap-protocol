4a34a147ecaf46b178a14329a62d37159088e05a
oritGreenvald <121660885+oritlu10@users.noreply.github.com>
2024-06-20 11:53:11 +0300

    Update math.sol

--- src/libraries/math.sol
+++ src/libraries/math.sol
@@ -1,22 +1,22 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
-
+pragma solidity ^0.8.24;
+import {State} from "src/contracts/CapStorage.sol";
 library Math {
     uint256 public constant BPS_DIVIDER = 10000;
 
-    function calculateCLPAmount(uint256 amount, uint256 clpSupply, uint256 balance) external pure returns (uint256) {
+    function calculateCLPAmount(State storage state, uint256 amount, uint256 clpSupply, uint256 balance) external pure returns (uint256) {
         return balance == 0 || clpSupply == 0 ? amount : amount * clpSupply / balance;
     }
 
-    function calculateFeeAmount(uint256 amount, uint256 feePercentage) external pure returns (uint256) {
+    function calculateFeeAmount(State storage state, uint256 amount, uint256 feePercentage) external pure returns (uint256) {
         return amount * feePercentage / BPS_DIVIDER;
     }
 
-    function calculateAmountMinusFee(uint256 amount, uint256 feeAmount) external pure returns (uint256) {
+    function calculateAmountMinusFee(State storage state, uint256 amount, uint256 feeAmount) external pure returns (uint256) {
         return amount - feeAmount;
     }
 
-    function calculateAmountToSendPool(uint256 bufferBalance, uint256 lastPaid, uint256 bufferPayoutPeriod) external view returns (uint256) {
+    function calculateAmountToSendPool(State storage state, uint256 bufferBalance, uint256 lastPaid, uint256 bufferPayoutPeriod) external view returns (uint256) {
         uint256 amountToSendPool = bufferBalance * (block.timestamp - lastPaid) / bufferPayoutPeriod;
         return amountToSendPool > bufferBalance ? bufferBalance : amountToSendPool;
     }
