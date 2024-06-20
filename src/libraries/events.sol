752d459a4b0ce254b686c06fbb6c48e985df19b9
oritGreenvald <121660885+oritlu10@users.noreply.github.com>
2024-06-20 11:52:47 +0300

    Create events.sol

---
+++ src/libraries/events.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+library Events {
+    // Pool Events
+    event AddLiquidity(address indexed user, uint256 amount, uint256 clpAmount, uint256 poolBalance);
+    event RemoveLiquidity(address indexed user, uint256 amount, uint256 feeAmount, uint256 clpAmount, uint256 poolBalance);
+    event FeePaid(address indexed user, string market, uint256 fee, uint256 poolFee, bool isLiquidation);
+    event GovernanceUpdated(address indexed oldGov, address indexed newGov);
+    event PoolPayIn(address indexed user, string market, uint256 amount, uint256 bufferToPoolAmount, uint256 poolBalance, uint256 bufferBalance);
+    event PoolPayOut(address indexed user, string market, uint256 amount, uint256 poolBalance, uint256 bufferBalance);
+}
