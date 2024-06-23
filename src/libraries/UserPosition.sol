2d13186ac34dde255c3f8b46c483a3dc48f9670a
MiriTenenboim <miryamt@darkeychana.com>
2024-06-23 12:37:59 +0300

    create function getUserPositions

--- src/libraries/UserPosition.sol
+++ src/libraries/UserPosition.sol
@@ -1,19 +1,23 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.24;
 
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "forge-std/console.sol";
-
-import {State} from "../contracts/CapStorage.sol";
-import {Position} from "../contracts/CapStorage.sol";
+import { State, PositionData } from "../contracts/CapStorage.sol";
+import { Position } from "../contracts/CapStorage.sol";
 
 library UserPosition {
 
     function getUserPositions(State storage state, address user) external view returns (Position[] memory _positions) {
-    //     uint256 length = state.positionData.positionKeysForUser[user].length();
-    //     _positions = new Position[](length);
-    //     for (uint256 i = 0; i < length; i++) {
-    //         _positions[i] = state.positionData.positions[state.positionData.positionKeysForUser[user].at(i)];
-    //     }
+        EnumerableSet.Bytes32Set storage userPositions = state.positionData.positionKeysForUser[user];
+        uint256 length = EnumerableSet.length(userPositions);
+        _positions = new Position[](length);
+
+        for (uint256 i = 0; i < length; i++) {
+            bytes32 positionKey = EnumerableSet.at(userPositions, i);
+            _positions[i] = state.positionData.positions[positionKey];
+        }
+
         return _positions;
     }
 }
