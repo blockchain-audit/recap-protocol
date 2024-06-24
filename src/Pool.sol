a3b9fde5de80724b4da788260b3eaab157d5c296
chanaKastner <chana64065@gmail.com>
2024-06-24 12:27:25 +0300

    add debitTraderProfit()

--- src/Pool.sol
+++ src/Pool.sol
@@ -6,31 +6,30 @@ import "./interfaces/IPool.sol";
 import {Liquidity} from "src/libraries/Liquidity.sol";
 import {CreditTraderLoss} from "src/libraries/CreditTraderLoss.sol";
 import {CreditFee} from "src/libraries/CreditFee.sol";
+import {DebitTraderProfit} from "./libraries/DebitTraderProfit.sol";
 
 contract Pool is IPool, CapStorage {
+
     using Liquidity for State;
     using CreditTraderLoss for State;
     using CreditFee for State;
+    using DebitTraderProfit for State;
 
     function addLiquidity(uint256 amount) public {
         state.validateAddLiquidity(amount);
         state.executeAddLiquidity(amount);
     }
 
-    function addLiquidityThroughUniswap(address tokenIn, uint256 amountIn, uint256 amountOutMin, uint24 poolFee)
-        public
-        payable
-    {
-        state.validateAddLiquidityThroughUniswap(tokenIn, amountIn, poolFee);
-        state.executeAddLiquidityThroughUniswap(tokenIn, amountIn, amountOutMin, poolFee);
+    function addLiquidityThroughUniswap(address tokenIn, uint256 amountIn, uint256 amountOutMin, uint24 poolFee) public payable {
+            state.validateAddLiquidityThroughUniswap(tokenIn, amountIn, poolFee);
+            state.executeAddLiquidityThroughUniswap(tokenIn, amountIn, amountOutMin, poolFee);     
     }
 
     function removeLiquidity(uint256 amount) public {
         state.validateRemoveLiquidity(amount);
-        state.executeRemoveLiquidity(amount);
+        state.executeRemoveLiquidity(amount);       
     }
-
-    function creditFee(string memory market, uint256 fee, bool isLiquidation) external {
+    function creditFee(string memory market, uint256 fee, bool isLiquidation) external{
         state.validateCreditFee(fee);
         state.executeCreditFee(market, fee, isLiquidation);
     }
@@ -40,9 +39,12 @@ contract Pool is IPool, CapStorage {
         state.executeCreditTraderLoss(market, amount);
     }
 
-    function debitTraderProfit(address user, string memory market, uint256 amount) external {}
+    function debitTraderProfit(address user, string memory market, uint256 amount) external {
+        state.validateDebitTraderProfit(user, amount);
+        state.executeDebitTraderProfit(user, market, amount);        
+    }
 
     function link(address _trade, address _store, address _treasury) external {}
 
     function updateGov(address _gov) external {}
-}
+} 
