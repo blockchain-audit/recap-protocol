a2b3419027e01c158d79b4f438e7188d7179a48c
RacheliPaley <racheli9467@gmail.com>
2024-06-24 13:53:23 +0300

    test  calculateAmountMinusFee

---
+++ src/tests/math_test.t.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.24;
+
+// import {Math} from "@src/libraries/Math.sol";
+import {Math} from "src/libraries/Math.sol";
+
+import {Test} from "forge-std/Test.sol";
+
+contract MathTest is Test {
+    function test_calculateAmountMinusFee() public {
+        uint256 BPS_DIVIDER = 10000;
+        uint256 amount = 100;
+        uint256 feePercentage = 10;
+     
+        uint256 result = Math.calculateAmountMinusFee( amount, feePercentage);
+        assertGt(result, amount, "its not correct");
+    }
+}
