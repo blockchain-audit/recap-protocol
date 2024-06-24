edf6fc942ea52200ffce23301bf97e1abae61163
chanaKastner <chana64065@gmail.com>
2024-06-24 13:17:48 +0300

    resolve conflicts

--- src/contracts/Pool.sol
+++ src/contracts/Pool.sol
@@ -2,18 +2,11 @@
 pragma solidity ^0.8.24;
 
 import "./CapStorage.sol";
-<<<<<<< HEAD:src/Pool.sol
 import "./interfaces/IPool.sol";
 import {Liquidity} from "src/libraries/Liquidity.sol";
 import {CreditTraderLoss} from "src/libraries/CreditTraderLoss.sol";
 import {CreditFee} from "src/libraries/CreditFee.sol";
 import {DebitTraderProfit} from "./libraries/DebitTraderProfit.sol";
-=======
-import "../interfaces/IPool.sol";
-import {Liquidity} from "../libraries/poolActions/Liquidity.sol";
-import {CreditTraderLoss} from "../libraries/poolActions/CreditTraderLoss.sol";
-import {CreditFee} from "../libraries/poolActions/CreditFee.sol";
->>>>>>> 1d7c77b5762a399ac1609af858a2e6d250ed33e0:src/contracts/Pool.sol
 
 contract Pool is IPool, CapStorage {
 
@@ -21,11 +14,7 @@ contract Pool is IPool, CapStorage {
     using Liquidity for uint256;
     using CreditTraderLoss for State;
     using CreditFee for State;
-<<<<<<< HEAD:src/Pool.sol
     using DebitTraderProfit for State;
-=======
-    // using DebitTraderProfit for State;
->>>>>>> 1d7c77b5762a399ac1609af858a2e6d250ed33e0:src/contracts/Pool.sol
 
     function addLiquidity(uint256 amount) public {
         amount.validateAddLiquidity();
@@ -52,13 +41,8 @@ contract Pool is IPool, CapStorage {
     }
 
     function debitTraderProfit(address user, string memory market, uint256 amount) external {
-<<<<<<< HEAD:src/Pool.sol
         state.validateDebitTraderProfit(user, amount);
         state.executeDebitTraderProfit(user, market, amount);        
-=======
-        // state.validateDebitTraderProfit(user, amount);
-        // state.executeDebitTraderProfit(user, market, amount);        
->>>>>>> 1d7c77b5762a399ac1609af858a2e6d250ed33e0:src/contracts/Pool.sol
     }
 
     function link(address _trade, address _store, address _treasury) external {}
